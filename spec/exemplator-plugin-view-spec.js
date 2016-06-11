8928fe3e4dfa4fad08bb1896759f629d4b7843cd
Valentin Rublack <valentin.rublack@gmail.com>
2016-06-11 15:46:58 +0200

    Initial commit

---
+++ spec/exemplator-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ExemplatorPluginView from '../lib/exemplator-plugin-view';
+
+describe('ExemplatorPluginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
