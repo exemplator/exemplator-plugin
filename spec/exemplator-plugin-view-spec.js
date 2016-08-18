53f3a39b14a6593c31f7a8e1d6b68f44aa84e559
Julian Brendl <julianbrendl@gmail.com>
2016-08-18 18:02:19 +0200

    added image

--- spec/exemplator-plugin-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import ExemplatorPluginView from '../lib/exemplator-plugin-view';
-
-describe('ExemplatorPluginView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
